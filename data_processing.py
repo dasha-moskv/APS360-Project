1503922e8afc20efa768143fe8b8a4e933280408
Hao <hao.xiang.yang2000@gmail.com>
2022-02-27 01:21:30 -0500

    Wrote some code to test load audio file, change it to 10s, and passing it through a spectrogram

--- data_processing.py
+++ data_processing.py
@@ -1,10 +1,17 @@
 #Data processing code
 #Codes are taken inspiration from Towards AI source [7] in project proposal
+#to download mp3 files from tatoeba, use https://audio.tatoeba.org/sentences/{lang}/{id}.mp3
 
+import os
 import numpy as np
 import librosa
+from librosa.display import waveshow
+from pyparsing import srange
+import soundfile
+import imageio
+import matplotlib.pyplot as plt
 
-sample_rate = 10000 #arbitrary, most english phonemes are 3kHz of bandwidth
+sr = 10000 #arbitrary, most english phonemes are 3kHz of bandwidth
 
 #chose so we could use alexnet.features and makes things easier
 image_width = 224
@@ -16,20 +23,20 @@ def add_noise(audio_file, gain):
     return audio_file + noise
 
 def load_audio_file(path):
-    audio_file, _ = librosa.load(path, sr=sample_rate)
-    return audio_file
+    audio_file, sample_rate = librosa.load(path)
+    return audio_file, sample_rate
 
-def change_to_10_s(audio_file):
+def change_to_10_s(audio_file, sample_rate):
     target_len = 10*sample_rate
-    audio_file = np.concatenate([audio_file]*3, axis = 0)
+    #audio_file = np.concatenate([audio_file]*10 axis = 0)
     audio_file = audio_file[0:target_len]
     return audio_file
 
-def spectrogram(audio_file):
+def spectrogram(audio_file, sample_rate):
     #compute Mel-scaled spectrogram image
     width = audio_file.shape[0]
     
-    spec = librosa.feature.melspectrogram(audio_file,n_mels=image_height, hop_length=int(width))
+    spec = librosa.feature.melspectrogram(audio_file, sr=sample_rate)
     image = librosa.core.power_to_db(spec) #log image
     #convert to np matrix
     image_np = np.asmatrix(image)
@@ -41,8 +48,49 @@ def spectrogram(audio_file):
 def to_png(image_float):
     #range (0,1) to (0,255)
     image_float_255 = image_float*255.0
-    image_int = image_float_255.astype(numpy.uint8)
+    image_int = image_float_255.astype(np.uint8)
     
     return image_int
     
+def augment_audio_file(source_path):
+    audio_segment, sample_rate = load_audio_file(source_path)
+    audio_segment_with_noise = add_noise(audio_segment, 0.005)
+    path = os.path.splitext(source_path)[0] + '_augmented_noise.wav'
+    soundfile.write(path, audio_segment_with_noise, sample_rate)
+
+def audio_to_image(audio_file, destination_folder):
+    image_file = destination_folder + '.png'
+    audio, sample_rate = load_audio_file(audio_file)
+    audio_fixed = change_to_10_s(audio, sr)
+    if np.count_nonzero(audio_fixed) != 0:
+        spectro = spectrogram(audio_fixed)
+        spectro_img = to_png(spectro)
+        imageio.imwrite(image_file, spectro_img)
+        return spectro
     
+###Testing###
+
+#augment a sample .wav file
+path1 = './Languages/English_audio/English_test_file.wav'
+#augment_audio_file(path)
+
+#convert to spectrogram
+path2 = './Languages/English_audio/English_test_file_augmented_noise.wav'
+#audio_to_image(path, './Languages/English/test')
+
+audio, sample = load_audio_file(path1)
+waveshow(audio, sr=sample)
+plt.title('Raw Audio File')
+plt.show()
+
+audio_fixed = change_to_10_s(audio, sr)
+waveshow(audio_fixed, sr=sr)
+plt.title('Raw Audio File fixed')
+plt.show()
+
+
+spectro = spectrogram(audio_fixed,sr)
+plt.imshow(spectro, origin='lower', aspect='auto')
+plt.title('Spectrogram')
+plt.show()
+spectro.shape
