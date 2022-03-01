646df5021029201142a228e6b820a919a1b5791b
Hao <hao.xiang.yang2000@gmail.com>
2022-03-01 00:43:29 -0500

    Produced a 224x224 image success

--- data_processing.py
+++ data_processing.py
@@ -9,7 +9,9 @@ from librosa.display import waveshow
 from pyparsing import srange
 import soundfile
 import imageio
+import matplotlib
 import matplotlib.pyplot as plt
+from torch import fmax
 
 sr = 10000 #arbitrary, most english phonemes are 3kHz of bandwidth
 
@@ -29,19 +31,20 @@ def load_audio_file(path):
 
 def change_to_10_s(audio_file, sample_rate):
     target_len = 10*sample_rate
-    #audio_file = np.concatenate([audio_file]*10 axis = 0)
+    audio_file = np.concatenate([audio_file]*10, axis = 0)
     audio_file = audio_file[0:target_len]
     return audio_file
 
 #ignore the warnings when it does melspectrogram
 def spectrogram(audio_file, sample_rate):
     #compute Mel-scaled spectrogram image
-    width = audio_file.shape[0]
+    print(audio_file.shape)
     
-    spec = librosa.feature.melspectrogram(audio_file, sr=sample_rate)
+    spec = librosa.feature.melspectrogram(audio_file, sr=sample_rate, n_mels=image_height)
     image = librosa.core.power_to_db(spec) #log image
     #convert to np matrix
     image_np = np.asmatrix(image)
+
     #normalize and scale
     image_np_scaled_temp = (image_np - np.min(image_np))
     image_np_scaled = image_np_scaled_temp/np.max(image_np_scaled_temp)
@@ -63,12 +66,15 @@ def augment_audio_file(source_path):
 def audio_to_image(audio_file, sample_rate, destination_folder):
     image_file = destination_folder + '.png'
     audio, sample_rate = load_audio_file(audio_file)
-    audio_fixed = change_to_10_s(audio, sr)
+    audio_fixed = change_to_10_s(audio, sample_rate)
     if np.count_nonzero(audio_fixed) != 0:
-        spectro = spectrogram(audio_fixed, sample_rate)
+        spectro = spectrogram(audio_fixed, sr)
         spectro_img = to_png(spectro)
-        imageio.imwrite(image_file, spectro_img)
-        return spectro
+        #spectro_tuple = (spectro_img,spectro_img,spectro_img)
+        #spectro_RGB = np.vstack(spectro_tuple)
+        #imageio.imwrite(image_file, spectro_RGB)
+        matplotlib.image.imsave(image_file, spectro_img)
+        return spectro_img
     
 ###Testing###
 
@@ -79,21 +85,27 @@ path1 = './Languages/English_audio/English_test_file.wav'
 #convert to spectrogram
 path2 = './Languages/English_audio/English_test_file_augmented_noise.wav'
 #audio_to_image(path, './Languages/English/test')
-
+'''
 audio, sample = load_audio_file(path1)
 waveshow(audio, sr=sample)
 plt.title('Raw Audio File')
 #plt.show()
 
-audio_fixed = change_to_10_s(audio, sr)
+audio_fixed = change_to_10_s(audio, sample)
 waveshow(audio_fixed, sr=sr)
 plt.title('Raw Audio File fixed')
 #plt.show()
 
 
 spectro = spectrogram(audio_fixed,sr)
+spectro = to_png(spectro)
+#spectro = librosa.display.specshow(audio_fixed, sr=sr)
+#spectro.savefig('test.png')
+
 plt.imshow(spectro, origin='lower', aspect='auto')
 plt.title('Spectrogram')
-#plt.show()
+plt.show()
 print(spectro.shape) #for now the shape is arbitrary, we need to make it so it's consistent throughout all data
-audio_to_image(path2, sr, './Languages/English/test')
+'''
+spectro = audio_to_image(path2, sr, './Languages/English/test')
+print(spectro.shape)
